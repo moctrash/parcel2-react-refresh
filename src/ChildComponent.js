e4888ef5270cd468f3355244a57e5cec1b86d33c
Ameen Rashad <ameenrashadv@gmail.com>
2020-06-30 23:22:37 +0530

    react-refrsh works in electron

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 
 export const ChildComponent = () => {
-    const [ count, setCount ] = useState(0);
-    return <p>Child with child state: {count} <button onClick={() => setCount(c => c + 1)}>Increment</button></p>
-}
+  const [count, setCount] = useState(0);
+  return (
+    <p>
+      Child with child state: {count}{" "}
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+    </p>
+  );
+};
